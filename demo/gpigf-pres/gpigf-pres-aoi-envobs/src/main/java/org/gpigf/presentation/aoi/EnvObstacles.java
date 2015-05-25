bc05f610256333aee973c13f6b833d7e762f8280
Tom Bruggenwirth <tb624@york.ac.uk>
2015-05-25 14:12:55 +0100

    Adding ability to save environmental obstacles
    Environmental obstacles can now be saved. When expanding, the target
    area now grows more slowly in areas where environmental obstacles are
    present. Works in conjunction with the road growth.

--- demo/gpigf-pres/gpigf-pres-aoi-envobs/src/main/java/org/gpigf/presentation/aoi/EnvObstacles.java
+++ demo/gpigf-pres/gpigf-pres-aoi-envobs/src/main/java/org/gpigf/presentation/aoi/EnvObstacles.java
@@ -5,6 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import org.geotools.process.factory.DescribeProcess;
 import org.geotools.process.factory.DescribeResult;
 import org.geotools.process.factory.StaticMethodsProcessFactory;
 import org.geotools.text.Text;
+import org.postgis.PGgeometry;
 
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryCollection;
@@ -27,28 +29,68 @@ public class EnvObstacles extends StaticMethodsProcessFactory<EnvObstacles>
 		super(Text.text("Environmental Obstacles"), "gpigf", EnvObstacles.class);
 	}
 	
+	@DescribeProcess(title = "saveEnvObstacles", description = "Save environmental obstacles to the database")
+	@DescribeResult(description = "Saved environmental obstacle")
+	public static GeometryCollection saveEnvObstacles(
+			@DescribeParameter(name = "target_areas", description = "Polygon of environmental obstacle(s)") GeometryCollection target_areas) {
+		
+		if (target_areas == null || target_areas.getNumGeometries() == 0) {
+			throw new IllegalArgumentException("target_areas is null or zero in size");
+		}
+		
+		startLogger();
+		startDBConnection();
+		
+		try {
+			PreparedStatement statement = conn.prepareStatement("INSERT into save (large_poly) VALUES (?)");
+			
+			for (int i = 0; i < target_areas.getNumGeometries(); ++i) {
+				Geometry geometry = target_areas.getGeometryN(i);
+	
+				statement.setObject(1, new PGgeometry(geometry.toText()));
+	  
+				if (statement.executeUpdate() > 0) {
+					out.println("Successful insert");
+				} else {
+					out.println("Failed insert");
+				}
+			}
+			
+			statement.close();
+		} catch (Exception e) {
+			e.printStackTrace(out);
+		}
+		
+		return target_areas;
+	}
+	
 	@DescribeProcess(title = "processEnvObstacles", description = "Calculates new size of target areas based upon environmental obstacles")
 	@DescribeResult(description = "Modified target areas")
 	public static GeometryCollection processEnvObstacles(
 			@DescribeParameter(name = "target_areas", description = "Target area polygon") GeometryCollection target_areas,
 			@DescribeParameter(name = "road_growth", description = "Growth along roads") int road_growth,
 			@DescribeParameter(name = "area_growth", description = "Polygon Growth") int area_growth,
+			@DescribeParameter(name = "env_obstacle_growth", description = "Growth in environmental obstacles") int env_obstacle_growth,
 			@DescribeParameter(name = "ext_target_area", description = "Distance to check for roads") int ext_target_area) {
 		
 		if (target_areas == null || target_areas.getNumGeometries() == 0) {
 			throw new IllegalArgumentException("target_areas is null or zero in size");
 		}
 		
-		GeometryCollection roads = GetRoads(target_areas);
+		startLogger();
+		startDBConnection();
+		
+		GeometryCollection roads = getRoads(target_areas.getFactory());
+		GeometryCollection envObstacles = getEnvironmentalObstacles(target_areas.getFactory());
+		
 		List<Geometry> geometryOutput = new ArrayList<Geometry>();
 		
 		for (int i = 0; i < target_areas.getNumGeometries(); i++) {
 			Geometry area = target_areas.getGeometryN(i);
-			Geometry bufferedArea = area.buffer(area_growth,5);
-			Geometry largerArea = area.buffer(ext_target_area,5);
+			Geometry largerArea = area.buffer(ext_target_area, 5);
 			
 			List<Geometry> output = new ArrayList<Geometry>();
-			output.add(bufferedArea);
+			out.println("Detecting road intersections");
 			
 			for (int j = 0; j < roads.getNumGeometries(); j++) {
 				Geometry road = roads.getGeometryN(j);
@@ -56,20 +98,94 @@ public class EnvObstacles extends StaticMethodsProcessFactory<EnvObstacles>
 					Geometry intersection = largerArea.intersection(road);
 					Geometry bufferedRoad = intersection.buffer(road_growth, 5);
 					output.add(bufferedRoad);
+					out.println("Road intersection detected");
 				}
 			}
 			
-			Geometry union = area.getFactory().createGeometryCollection(GeometryFactory.toGeometryArray(output)).union();
-			geometryOutput.add(union);
+			List<Geometry> obstacleIntersections = new ArrayList<Geometry>();
+			out.println("Detecting environmental obstacle intersections");
+			
+			for (int j = 0; j < envObstacles.getNumGeometries(); j++) {
+				Geometry obstacle = envObstacles.getGeometryN(j);
+				if (area.intersects(obstacle)) {
+					Geometry intersection = area.intersection(obstacle);
+					obstacleIntersections.add(intersection);
+					out.println("Environmental obstacle intersection detected");
+				}
+			}
+			
+			if (obstacleIntersections.size() > 0) {
+				Geometry envObstacleIntersections = toGeometryCollection(area, obstacleIntersections).union();
+				Geometry remaining = area.difference(envObstacleIntersections);
+				
+				// Grow the regular area and the areas in environmental obstacles at different rates
+				output.add(envObstacleIntersections.buffer(env_obstacle_growth, 5));
+				output.add(remaining.buffer(area_growth, 5));
+			} else {
+				output.add(area.buffer(area_growth, 5));
+			}
+			
+			geometryOutput.add(toGeometryCollection(area, output).union());
 		}
 		
-		return target_areas.getFactory().createGeometryCollection(GeometryFactory.toGeometryArray(geometryOutput));
+		return toGeometryCollection(target_areas, geometryOutput);
 	}
 	
 	private static java.sql.Connection conn;
 	private static PrintWriter out = null;
 	
-	public static GeometryCollection GetRoads(GeometryCollection target_areas) {
+	private static GeometryCollection toGeometryCollection(Geometry geometry, List<Geometry> geometryList) {
+		return geometry.getFactory().createGeometryCollection(GeometryFactory.toGeometryArray(geometryList));
+	}
+	
+	private static GeometryCollection getEnvironmentalObstacles(GeometryFactory factory) {
+		out.println("Attempting to get env obstacles");
+		List<Geometry> output = getGeometries("SELECT st_astext(large_poly) as poly FROM save");
+		out.println("Finished getting env obstacles");
+		return factory.createGeometryCollection(GeometryFactory.toGeometryArray(output));
+	}
+	
+	private static GeometryCollection getRoads(GeometryFactory factory) {
+		out.println("Attempting to get roads");
+		List<Geometry> output = getGeometries("SELECT st_astext(st_transform(ST_LineMerge(geom),3857)) FROM nyc_roads");
+		out.println("Finished getting roads");
+		return factory.createGeometryCollection(GeometryFactory.toGeometryArray(output));
+	}
+
+	private static List<Geometry> getGeometries(String sql) {
+		List<Geometry> output = new ArrayList<Geometry>();
+		
+		try {
+			Statement query = conn.createStatement();
+			ResultSet results = query.executeQuery(sql);
+			
+			while (results.next()) {
+				WKTReader wktReader = new WKTReader();
+				Geometry geometry = wktReader.read((String)results.getObject(1));
+				output.add(geometry);
+				out.println("Read from database: " + geometry.toText());
+			}
+			
+			query.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return output;
+	}
+
+	private static void startDBConnection() {
+		try {
+			Class.forName("org.postgresql.Driver"); 
+			String url = "jdbc:postgresql://localhost:5432/RoadsDB"; 
+			conn = DriverManager.getConnection(url, "postgres", "geoserver"); 
+			((org.postgresql.PGConnection)conn).addDataType("geometry",Class.forName("org.postgis.PGgeometry"));
+		} catch (Exception e ) {
+			e.printStackTrace(out);
+		}
+	}
+	
+	private static void startLogger() {
 		try {
 			if (out == null) {
 				out = new PrintWriter(new BufferedWriter(new FileWriter("C:\\Users\\tbrug_000\\Desktop\\out.log", false)), true);
@@ -79,40 +195,6 @@ public class EnvObstacles extends StaticMethodsProcessFactory<EnvObstacles>
 		}
 		
 		out.println("log started");
-
-		try {
-			Class.forName("org.postgresql.Driver"); 
-			String url = "jdbc:postgresql://localhost:5432/RoadsDB"; 
-			conn = DriverManager.getConnection(url, "postgres", "geoserver"); 
-			((org.postgresql.PGConnection)conn).addDataType("geometry",Class.forName("org.postgis.PGgeometry"));
-		} catch (Exception e ) { 
-			e.printStackTrace(out);
-		}
-		
-		List output = new ArrayList();
-		
-		try {
-			Statement qs = conn.createStatement();
-			ResultSet r = qs.executeQuery("select st_astext(st_transform(ST_LineMerge(geom),3857)) from nyc_roads");
-			
-			while (r.next()) {
-				String wkt = (String)r.getObject(1);
-	
-				WKTReader wktReader = new WKTReader();
-				Geometry geo = wktReader.read(wkt);
-	
-				out.println("read from db : " + geo.toText());
-				output.add(geo);
-			}
-			
-			qs.close();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-
-		out.println("done");
-		
-		// The param is used to get the right factory
-		return target_areas.getFactory().createGeometryCollection(GeometryFactory.toGeometryArray(output));
 	}
+	
 }
--- demo/webapp-pres/src/app/additional/EnvObstacles.js
+++ demo/webapp-pres/src/app/additional/EnvObstacles.js
@@ -6,76 +6,101 @@
  * @require OpenLayers/Handler/Polygon.js
  * @require OpenLayers/Handler/Path.js
  * @require OpenLayers/WPSClient.js
- *
  * @require gpigf/Tool.js
  */
 
 var attractors = Ext.extend(gpigf.plugins.Tool, {
-
-  ptype: 'app_env_obstacles',
+    ptype: 'app_env_obstacles',
   
-  /** Initialization of the plugin */
-  init: function(target) {
-    attractors.superclass.init.apply(this, arguments);
+    /** Initialization of the plugin */
+    init: function(target) {
+        attractors.superclass.init.apply(this, arguments);
 
-    // Create a WPSClient instance for use with the local GeoServer
-    this.wpsClient = new OpenLayers.WPSClient({
-      servers: {
-        local: '/geoserver/wps'
-      }
-    });
-
-    // Add action buttons when the viewer is ready
-    target.on('ready', function() {
-      // Get a reference to the vector layer from app.js
-      this.layer = target.getLayerRecordFromMap({
-        name: 'sketch',
-        source: 'ol'
-      }).getLayer();
-      // Some defaults
-      var actionDefaults = {
-        map: target.mapPanel.map,
-        enableToggle: true,
-        toggleGroup: this.ptype,
-        allowDepress: true
-      };
-      this.addActions([
-        new GeoExt.Action(Ext.apply({
-          text: 'Load Roads',
-          control: new OpenLayers.Control.DrawFeature(
-            this.layer, OpenLayers.Handler.Polygon, {
-            eventListeners: {
-              featureadded: this.processEnvObstacles,
-              scope: this
+        // Create a WPSClient instance for use with the local GeoServer
+        this.wpsClient = new OpenLayers.WPSClient({
+            servers: {
+                local: '/geoserver/wps'
             }
-          })
-        }, actionDefaults))
-      ]);
-    }, this);
-  },
+        });
 
-  processEnvObstacles: function(evt) {
-    var polys = evt.feature;
+        // Add action buttons when the viewer is ready
+        target.on('ready', function() {
+            // Get a reference to the vector layer from app.js
+            this.layer = target.getLayerRecordFromMap({
+                name: 'sketch',
+                source: 'ol'
+            }).getLayer();
+            // Some defaults
+            var actionDefaults = {
+                map: target.mapPanel.map,
+                enableToggle: true,
+                toggleGroup: this.ptype,
+                allowDepress: true
+            };
+            this.addActions([
+                new GeoExt.Action(Ext.apply({
+                    text: 'Grow with Environmetal Obstacles',
+                    control: new OpenLayers.Control.DrawFeature(
+                        this.layer, OpenLayers.Handler.Polygon, {
+                        eventListeners: {
+                            featureadded: this.processEnvObstacles,
+                            scope: this
+                        }
+                    })
+                }, actionDefaults)),
+                new GeoExt.Action(Ext.apply({
+                    text: 'Save Environmental Obstacle',
+                    control: new OpenLayers.Control.DrawFeature(
+                        this.layer, OpenLayers.Handler.Polygon, {
+                        eventListeners: {
+                            featureadded: this.saveEnvObstacles,
+                            scope: this
+                        }
+                    })
+                }, actionDefaults))
+            ]);
+        }, this);
+    },
+
+    processEnvObstacles: function(evt) {
+        var polys = evt.feature;
   
-    this.wpsClient.execute({
-          server: 'local',
-          process: 'gpigf:processEnvObstacles',
-          inputs: {
-            target_areas: polys,
-            road_growth: 20,
-            area_growth: 5,
-            ext_target_area: 50
-          },
-          success: this.addResult,
-          scope: this
-    });
+        this.wpsClient.execute({
+            server: 'local',
+            process: 'gpigf:processEnvObstacles',
+            inputs: {
+                target_areas: polys,
+                road_growth: 20,
+                area_growth: 5,
+                ext_target_area: 50,
+                env_obstacle_growth: 1
+            },
+            success: this.addResult,
+            scope: this
+        });
       
-    this.layer.removeFeatures(polys);
-},
+        this.layer.removeFeatures(polys);
+    },
+    
+    saveEnvObstacles: function(evt) {
+        var polys = evt.feature;
+  
+        this.wpsClient.execute({
+            server: 'local',
+            process: 'gpigf:saveEnvObstacles',
+            inputs: {
+                target_areas: polys
+            },
+            success: console.log('environmental obstacle saved'),
+            scope: this
+        });
+      
+        this.layer.removeFeatures(polys);
+    },
 
-addResult: function(outputs) {
-    this.layer.addFeatures(outputs.result);         
-}
+    addResult: function(outputs) {
+        this.layer.addFeatures(outputs.result);         
+    }
           
 });
 
