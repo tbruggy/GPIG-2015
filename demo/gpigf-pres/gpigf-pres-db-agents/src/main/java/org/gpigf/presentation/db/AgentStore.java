4a9dc48f36bbbc4c89b54d4b0c6d73c623c07ff7
jm876 <jayem876@gmail.com>
2015-05-28 10:58:30 +0100

    Updated agentDigArea java and added related js AgentPushPop.js

--- demo/gpigf-pres/gpigf-pres-db-agents/src/main/java/org/gpigf/presentation/db/AgentStore.java
+++ demo/gpigf-pres/gpigf-pres-db-agents/src/main/java/org/gpigf/presentation/db/AgentStore.java
@@ -12,10 +12,12 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryCollection;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.PrecisionModel;
 import com.vividsolutions.jts.geom.util.LineStringExtracter;
 import com.vividsolutions.jts.io.WKBReader;
@@ -38,13 +40,13 @@ public class AgentStore extends StaticMethodsProcessFactory<AgentStore> {
 		
 		try {
 			if (out == null) {
-				out = new PrintWriter(new BufferedWriter(new FileWriter("C:\\Users\\Jack\\Desktop\\out2.log", false)), true);
+				out = new PrintWriter(new BufferedWriter(new FileWriter("C:\\Users\\Jack\\Desktop\\out3.log", false)), true);
 			}
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
 		
-		out.println("agents: log started");
+		out.println("agents2: log started");
 
 		try {
 			Class.forName("org.postgresql.Driver"); 
@@ -63,42 +65,6 @@ public class AgentStore extends StaticMethodsProcessFactory<AgentStore> {
 			e.printStackTrace(out);
 		}
 	}
-
-	/*
-	@DescribeProcess(title = "push", description = "Pushes a geometry to the db")
-	@DescribeResult(description = "push geometries")
-	public static Geometry push(
-			@DescribeParameter(name = "target_areas", description = "Target area polygon") GeometryCollection target_areas) {
-
-		if (target_areas == null || target_areas.getNumGeometries() == 0)
-			throw new IllegalArgumentException("target_areas is null or zero in size");
-		
-		try {
-			PreparedStatement s = conn.prepareStatement("INSERT INTO save (srid, precision, large_poly) VALUES (?, ?, ?)");
-			
-			for (int i = 0; i < target_areas.getNumGeometries(); ++i) {
-				Geometry geo = target_areas.getGeometryN(i);
-				
-				
-				s.setInt(1, geo.getFactory().getSRID());
-				s.setString(2, geo.getFactory().getPrecisionModel().getType().toString());
-				s.setObject(3, new PGgeometry(geo.toText()));
-				
-				int rows = s.executeUpdate();
-	 
-				if (rows > 0) {
-					out.println("Successful insert");
-				} else {
-					out.println("Failed insert");
-				}
-			}
-			s.close();
-		} catch (Exception e) {
-			e.printStackTrace(out);
-		}
-
-		return target_areas;
-	}*/
 	
 	@DescribeProcess(title = "pushAgents", description = "Pushes agents to the db")
 	@DescribeResult(description = "push geometries")
@@ -122,9 +88,9 @@ public class AgentStore extends StaticMethodsProcessFactory<AgentStore> {
 				int rows = s.executeUpdate();
 	 
 				if (rows > 0) {
-					out.println("Successful insert");
+					//out.println("Successful insert");
 				} else {
-					out.println("Failed insert");
+					//out.println("Failed insert");
 				}
 			//}
 			s.close();
@@ -134,130 +100,6 @@ public class AgentStore extends StaticMethodsProcessFactory<AgentStore> {
 
 		return agents;
 	}
-	
-	/*
-	@DescribeProcess(title = "savePosition", description = "Stores the agent position to the db")
-	@DescribeResult(description = "push agents")
-	public static Geometry savePosition(
-			@DescribeParameter(name = "agentID", description = "The ID of the agent to store") int agentID,
-			@DescribeParameter(name = "posNum", description = "The number of this position in the sequence") int posNum,
-			@DescribeParameter(name = "geom", description = "The agent geometry at this position") Geometry geom) {
-
-		if (geom == null || geom.getNumGeometries() == 0)
-			throw new IllegalArgumentException("geom is null or zero in size");
-		if (posNum < 0)
-			throw new IllegalArgumentException("posNum is negative");
-		
-		try {
-			PreparedStatement s = conn.prepareStatement("INSERT INTO agentpositions (agentid, posnum, srid, precision, geom) VALUES (?,?,?,?,?)");
-			
-			
-				s.setInt(1, agentID);
-				s.setInt(2, posNum);
-				s.setInt(3, geom.getFactory().getSRID());
-				s.setString(4, geom.getFactory().getPrecisionModel().getType().toString());
-				s.setObject(5, new PGgeometry(geom.toText()));
-				
-				int rows = s.executeUpdate();
-	 
-				if (rows > 0) {
-					out.println("Successful insert");
-				} else {
-					out.println("Failed insert");
-				}
-			s.close();
-		} catch (Exception e) {
-			e.printStackTrace(out);
-		}
-
-		return geom;
-	}*/
-	
-	/*
-	@DescribeProcess(title = "pushAgents", description = "Stores an array of agents to the db")
-	@DescribeResult(description = "push agent")
-	public static Geometry pushAgents(
-			@DescribeParameter(name = "agentsArray", description = "An array of geometries") Geometry[] agentsArray) {
-
-		if (agentsArray == null || agentsArray.length == 0)
-			throw new IllegalArgumentException("geom is null or zero in size");
-		
-		try {
-			PreparedStatement s = conn.prepareStatement("INSERT INTO agents (agentid, posnum, srid, precision, geom) VALUES (?,?,?,?,?)");
-			
-			
-			s.setObject(1, new PGgeometry(agentsArray[0].toText()));
-			s.setObject(2, new PGgeometry(agentsArray[1].toText()));
-			s.setObject(3, new PGgeometry(agentsArray[2].toText()));
-			s.setObject(4, new PGgeometry(agentsArray[3].toText()));
-			s.setObject(5, new PGgeometry(agentsArray[4].toText()));
-				
-			int rows = s.executeUpdate();
-	 
-			if (rows > 0) {
-				out.println("Successful insert");
-			} else {
-				out.println("Failed insert");
-			}
-			s.close();
-		} catch (Exception e) {
-			e.printStackTrace(out);
-		}
-
-		return agentsArray[0];
-	}*/
-	
-	/*
-	@DescribeProcess(title = "popAgents", description = "Retrives all agent positions")
-	@DescribeResult(description = "popped agents")
-	public static Geometry popAgents(
-			@DescribeParameter(name = "posNum", description = "Position number to retrieve") int posNum) {
-
-		//if (target_areas == null || target_areas.getNumGeometries() == 0)
-		//	throw new IllegalArgumentException("target_areas is null or zero in size");
-		
-		List output = new ArrayList();
-		GeometryFactory f = null;
-		
-		try {
-			
-			//TODO here
-			Statement qs = conn.createStatement();
-			ResultSet r = qs.executeQuery("SELECT st_astext(agent1) as agent1,st_astext(agent2) as agent2,st_astext(agent3) as agent3,st_astext(agent4) as agent4,st_astext(agent5) as agent5, FROM agents;");
-			
-			//s.setInt(1, posNum);
-			
-			//ResultSet r = qs.executeQuery("SELECT srid,precision,st_astext(large_poly) as poly FROM save");
-			
-			
-			while (r.next()) {
-				if (r.getRow() == posNum){
-					if (f == null) {
-						int srid = r.getInt(1);
-						String precision = (String)r.getObject(2);
-					
-						f = new GeometryFactory(new PrecisionModel(), srid);
-					}
-					String wkt = (String)r.getObject(3);
-	
-					WKTReader wktReader = new WKTReader();
-					Geometry geo = wktReader.read(wkt);
-	
-					out.println("read from db : " + geo.toText());
-					output.add(geo);
-				}
-			}
-			
-			qs.close();
-		} catch (Exception e) {
-			e.printStackTrace(out);
-		}
-
-		out.println("done");
-
-		// The param is used to get the right factory
-		return f.createGeometryCollection(GeometryFactory.toGeometryArray(output));
-	}*/
 	
 	@DescribeProcess(title = "popPosition", description = "Pops a specific position of agents from the db")
 	@DescribeResult(description = "popped geometry")
@@ -282,12 +124,12 @@ public class AgentStore extends StaticMethodsProcessFactory<AgentStore> {
 						f = new GeometryFactory(new PrecisionModel(), srid);
 					}
 					String wkt = (String)r.getObject(2);
-					out.println(wkt);
-					out.println(r.getObject(2));
+					//out.println(wkt);
+					//out.println(r.getObject(2));
 					WKTReader wktReader = new WKTReader();
 					Geometry geo = wktReader.read(wkt);
 		
-					out.println("read from db : " + geo.toText());
+					//out.println("read from db : " + geo.toText());
 					//output.add(geo);
 					return (GeometryCollection) geo;
 				}
@@ -330,7 +172,7 @@ public class AgentStore extends StaticMethodsProcessFactory<AgentStore> {
 				WKTReader wktReader = new WKTReader();
 				Geometry geo = wktReader.read(wkt);
 	
-				out.println("read from db : " + geo.toText());
+				//out.println("read from db : " + geo.toText());
 				output.add(geo);
 			}
 			
@@ -344,100 +186,62 @@ public class AgentStore extends StaticMethodsProcessFactory<AgentStore> {
 		// The param is used to get the right factory
 		return f.createGeometryCollection(GeometryFactory.toGeometryArray(output));
 	}
+	
+	@DescribeProcess(title = "agentDigArea", description = "Subtracts an agents path from a target area, using old and new positions")
+	@DescribeResult(description = "Modified target areas")
+	public static GeometryCollection agentDigArea(
+			@DescribeParameter(name = "target_areas", description = "Target area polygon") GeometryCollection target_areas,
+			@DescribeParameter(name = "previous_positions", description = "Previous positions of agents") GeometryCollection previous_positions,
+			@DescribeParameter(name = "new_positions", description = "New positions of agents") GeometryCollection new_positions,
+			@DescribeParameter(name = "agent_vision", description = "Radius of agents vision") int agent_vision) 
+					throws IllegalArgumentException {
+		
+		out.println("Called");
+		out.flush();
 
-	/*
-	@DescribeProcess(title = "pop", description = "Pops a geometry from the db")
-	@DescribeResult(description = "popped geometry")
-	public static GeometryCollection pop(
-			@DescribeParameter(name = "target_areas", description = "Target area polygon") GeometryCollection target_areas) {
-
+		
+		if (target_areas == null || previous_positions == null || new_positions == null)
+			out.println("null");
+		
+		
+		out.println("target_areas : " + target_areas.toText());
+		out.println("previous_positions : " + previous_positions.toText());
+		out.println("new_positions : " + new_positions.toText());
+		out.println("agent_vision : " + agent_vision);
+		
 		if (target_areas == null || target_areas.getNumGeometries() == 0)
 			throw new IllegalArgumentException("target_areas is null or zero in size");
 		
-		List output = new ArrayList();
-		GeometryFactory f = null;
-
-		try {
-			Statement qs = conn.createStatement();
-			ResultSet r = qs.executeQuery("SELECT srid,precision,st_astext(large_poly) as poly FROM save");
+		
+		List<Geometry> paths = new ArrayList<Geometry>();
+		for (int i=0; i < previous_positions.getNumGeometries(); ++i){
+			Geometry previous_pos = previous_positions.getGeometryN(i);
+			Geometry next_pos = new_positions.getGeometryN(i);
+			Coordinate[] line_coords = new Coordinate[2];
+			line_coords[0]=previous_pos.getCoordinate();
+			line_coords[1]=next_pos.getCoordinate();
 			
-			while (r.next()) {
-				if (f == null) {
-					int srid = r.getInt(1);
-					String precision = (String)r.getObject(2);
-					
-					f = new GeometryFactory(new PrecisionModel(), srid);
-				}
-				String wkt = (String)r.getObject(3);
-	
-				WKTReader wktReader = new WKTReader();
-				Geometry geo = wktReader.read(wkt);
-	
-				out.println("read from db : " + geo.toText());
-				output.add(geo);
+			LineString path = new LineString(line_coords,new PrecisionModel(),0);
+			Geometry visioned_path = path.buffer(agent_vision,5);
+			paths.add(visioned_path);
+		}
+		
+		out.println("place 1");
+//		
+		List<Geometry> output = new ArrayList<Geometry>();
+//
+		for (int i = 0; i < target_areas.getNumGeometries(); ++i) {
+			Geometry new_area = target_areas.getGeometryN(i);
+			for (int j=0; j<paths.size(); ++j){
+				new_area = new_area.difference(paths.get(j));
 			}
-			
-			qs.close();
-		} catch (Exception e) {
-			e.printStackTrace(out);
+
+			output.add(new_area);
 		}
 
-		out.println("done");
+		out.println("place 2");
+		return target_areas.getFactory().createGeometryCollection(GeometryFactory.toGeometryArray(output));
+		//return target_areas;
+	}
 
-		// The param is used to get the right factory
-		return f.createGeometryCollection(GeometryFactory.toGeometryArray(output));
-	}*/
-	
-	/*
-	@DescribeProcess(title = "getNumPositions", description = "Returns the number of saved positions for the given agent")
-	@DescribeResult(description = "The number of saved positions for the given agent")
-	public static Integer getNumPositions(
-			@DescribeParameter(name = "agentID", description = "The ID of the agent") int agentID) {
-
-		// Initialise the count to 0
-		int count = 0;
-
-		try {
-			//Statement qs = conn.createStatement();
-			//ResultSet r = qs.executeQuery(
-			PreparedStatement s = conn.prepareStatement("SELECT COUNT(posNum) FROM Orders WHERE agentID=?;");
-			
-			s.setInt(1, agentID);
-			
-			ResultSet r = s.executeQuery();
-			
-			// Retrieve the first element in the (only) column
-			count = r.getInt(1);
-			
-			s.close();
-			
-			/*
-			while (r.next()) {
-				if (f == null) {
-					int srid = r.getInt(1);
-					String precision = (String)r.getObject(2);
-					
-					f = new GeometryFactory(new PrecisionModel(), srid);
-				}
-				String wkt = (String)r.getObject(3);
-	
-				WKTReader wktReader = new WKTReader();
-				Geometry geo = wktReader.read(wkt);
-	
-				out.println("read from db : " + geo.toText());
-				output.add(geo);
-			}
-			
-			qs.close();
-			*/
-	/*
-		} catch (Exception e) {
-			e.printStackTrace(out);
-		}
-
-		out.println("done");
-
-		// Return the count of positions
-		return count;
-	}*/
 }
---
+++ demo/webapp-pres/src/app/additional/AgentPushPop.js
@@ -0,0 +1,301 @@
+/**
+ * @require plugins/Tool.js
+ * @require GeoExt/widgets/Action.js
+ * @require OpenLayers/Control/SelectFeature.js
+ * @require OpenLayers/Control/DrawFeature.js
+ * @require OpenLayers/Control/DragFeature.js
+ * @require OpenLayers/Handler/Polygon.js
+ * @require OpenLayers/Handler/Path.js
+ * @require OpenLayers/WPSClient.js
+ */
+ 
+var agentNum = 1;
+
+var numPositions = -1;
+
+var Demo = Ext.extend(gxp.plugins.Tool, {
+
+    ptype: 'app_demo',
+    
+    /** Initialization of the plugin */
+    init: function(target) {
+        Demo.superclass.init.apply(this, arguments);
+
+        // Create a WPSClient instance for use with the local GeoServer
+        this.wpsClient = new OpenLayers.WPSClient({
+            servers: {
+                local: '/geoserver/wps'
+            }
+        });
+    
+        // Add action buttons when the viewer is ready
+        target.on('ready', function() {
+            // Get a reference to the vector layer from app.js
+            this.layer = target.getLayerRecordFromMap({
+                name: 'sketch',
+                source: 'ol'
+            }).getLayer();
+            // Some defaults
+            var actionDefaults = {
+                map: target.mapPanel.map,
+                enableToggle: true,
+                toggleGroup: this.ptype,
+                allowDepress: true
+            };
+			
+			
+			
+            this.addActions([
+			
+				// Action for adding an agent to the map
+                new GeoExt.Action(Ext.apply({
+                    text: 'Add Agent',
+                    control: new OpenLayers.Control.DrawFeature(
+                        this.layer, OpenLayers.Handler.Point,
+						{
+							eventListeners: {
+								featureadded: this.addToArray,
+								scope: this
+							}
+						}
+                    )
+                }, actionDefaults)),
+				
+				// Action for drawing a polygon on the map
+                new GeoExt.Action(Ext.apply({
+                    text: 'Draw Area',
+                    control: new OpenLayers.Control.DrawFeature(
+                        this.layer, OpenLayers.Handler.Polygon,
+						{
+							eventListeners:
+							{
+								featureadded: this.saveTargetArea,
+								scope: this
+							}
+						}
+                    )
+                }, actionDefaults)),
+				
+				// Action for dragging geometries
+                new GeoExt.Action(Ext.apply({
+                    text: 'Drag',
+                    control: new OpenLayers.Control.DragFeature(
+                        this.layer
+                    )
+                }, actionDefaults)),
+				
+				// Store Current Agents
+				new GeoExt.Action(Ext.apply({
+                    text: 'Push Agents',
+                    control: new OpenLayers.Control.Button({
+						 displayClass: "Demo", trigger: OpenLayers.Function.bind(this.pushAgents, this)
+					})
+                }, actionDefaults)),
+
+				// Action for resetting and displaying the agent positions
+				new GeoExt.Action(Ext.apply({
+                    text: 'Reset Positions',
+                    control: new OpenLayers.Control.Button({
+						displayClass: "Demo", trigger: OpenLayers.Function.bind(this.resetPosition, this)
+					})
+                }, actionDefaults)),
+				
+				// Action for displaying the next agent positions
+				new GeoExt.Action(Ext.apply({
+                    text: 'Next Positions',
+                    control: new OpenLayers.Control.Button({
+						displayClass: "Demo", trigger: OpenLayers.Function.bind(this.nextPosition, this)
+					})
+                }, actionDefaults)),
+				
+				// Action for removing from a target area using current and previous agent positions
+				new GeoExt.Action(Ext.apply({
+                    text: 'Dig',
+                    control: new OpenLayers.Control.SelectFeature(
+					this.layer,
+					{
+						  clickout: true, toggle: false,
+						  multiple: false, hover: false,
+						  toggleKey: "ctrlKey", // ctrl key removes from selection
+						  multipleKey: "shiftKey", // shift key adds to selection
+						  box: true,
+						  eventListeners: {
+							  featurehighlighted: this.dig,
+							  scope: this
+						  }
+					}
+				)
+                }, actionDefaults)),
+				
+            ]);
+        }, this);
+    },
+	
+	// Current agent positions
+	agentsArray : [],
+	
+	// Index used in the adding of agents to the array, before they are pushed
+	agentsArrayIndex : 1,
+	
+	// Position number used to iterate through positions in the database
+	positionNumber : 1,
+	
+	// The agent positions 1 timestep before the current ones
+	lastArray: [],
+	
+	
+	// Add a point to the agentsArray
+	addToArray: function(evt) {
+		this.agentsArray[this.agentsArrayIndex] = evt.feature;
+		this.agentsArrayIndex++;
+		
+		//alert(this.agentsArray);
+	},
+	
+	// Remove the agents (but add the area again)
+	removeAgents: function(evt) {
+		this.layer.removeAllFeatures();
+		this.layer.addFeatures(targetArea);
+	},
+	
+	// Save previous when a next position is loaded and displayed
+	// This automatically overwrites the previous 'savePrevious'
+	savePrevious: function(evt) {
+		this.lastArray = this.agentsArray;
+	},
+	
+	// Load the first position in the database
+	resetPosition: function(evt) {
+		this.removeAgents();
+		this.positionNumber = 1;
+		this.wpsClient.execute({
+            server: 'local',
+            process: 'gpigf:popPosition',
+            inputs: {
+                posNum: this.positionNumber
+            },
+            success: this.poppedResult,
+            scope: this
+        });
+	},
+	
+	// Loads the next position from the database
+	nextPosition: function(evt) {
+		this.removeAgents();
+		this.positionNumber++;
+		this.wpsClient.execute({
+            server: 'local',
+            process: 'gpigf:popPosition',
+            inputs: {
+                posNum: this.positionNumber
+            },
+            success: this.poppedResult,
+            scope: this
+        });
+	},
+	
+	// Retrieve the positions given by the position number
+	popPosition: function(evt) {
+		this.wpsClient.execute({
+            server: 'local',
+            process: 'gpigf:popPosition',
+            inputs: {
+                posNum: this.positionNumber
+            },
+            success: this.poppedResult,
+            scope: this
+        });
+	},
+	
+	// Push agents added using 'Add Agent' to the database
+	pushAgents: function(evt) {
+        this.wpsClient.execute({
+            server: 'local',
+            process: 'gpigf:pushAgents',
+            inputs: {
+                agents: this.agentsArray//agentsCol
+            },
+            success: this.pushedResult,
+            scope: this
+        });
+        //this.layer.removeFeatures([poly]);  // Remove this line for psuedo-heatmap styling!
+    },
+	
+	// Test button function - not currently used
+	doit: function(evt) {
+		alert("hi");
+		this.button.control.displayClass = "Demo";
+	},
+
+	// Display all positions stored in the database (not currently used)
+	popAgents: function(evt) {
+		var poly = evt.feature;
+        this.wpsClient.execute({
+            server: 'local',
+            process: 'gpigf:popAgents',
+            inputs: {
+                agents: poly
+            },
+            success: this.poppedResult,
+            scope: this
+        });
+        //this.layer.removeFeatures([poly]);  // Remove this line for psuedo-heatmap styling!
+    },
+	
+	// Remove a line from the area, using the positions in lastArray and agentsArray
+	dig: function(evt) {
+		var feature = evt.feature;
+		var featureArray = [feature];
+		var a = this.lastArray;
+		var b = this.agentsArray;
+		this.layer.removeFeatures(targetArea);
+		this.wpsClient.execute({
+            server: 'local',
+            process: 'gpigf:agentDigArea',
+            inputs: {
+				target_areas: evt.feature,
+                previous_positions: this.lastArray,
+				new_positions: this.agentsArray,
+				agent_vision: 10000
+            },
+            success: this.addResult,
+            scope: this
+        });
+	},
+	
+	// Called after a result is pushed - currently does nothing
+	pushedResult: function(outputs) {
+	},
+	
+	// Called after a result is popped, updates lastArray and agentsArray and displays new positions
+	poppedResult: function(outputs) {
+		if (outputs.result == undefined) {
+			console.log("undefined from pop");
+			return;
+		}
+		//alert(outputs.result);
+		this.lastArray = this.agentsArray;
+		this.agentsArray = outputs.result;
+		//for (int i; i < output.result.getNumGeometries(); i++) {
+			//this.agentsArray[i] = output.result.getGeomtryN(i);
+		//}
+		this.layer.addFeatures(outputs.result);
+	},
+
+    /** Helper function for adding process results to the vector layer */
+    addResult: function(outputs) {
+        this.layer.addFeatures(outputs.result);
+		targetArea = outputs.result[0];
+    },
+	
+	// Variable to save the target area
+	targetArea: null,
+	
+	// Saves the target area - used to prevent it from being removed
+	saveTargetArea:function(evt){
+		targetArea = evt.feature
+	}
+
+});
+
+Ext.preg(Demo.prototype.ptype, Demo);
