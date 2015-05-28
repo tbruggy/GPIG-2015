0c966abd33e05a417f84ae51bdde9f2ea97c8390
Paul Clothier <paulclothier09@gmail.com>
2015-05-28 22:09:26 +0100

    Changed DB name to RoadsDB

--- demo/gpigf-pres/gpigf-pres-db-agents/src/main/java/org/gpigf/presentation/db/AgentStore.java
+++ demo/gpigf-pres/gpigf-pres-db-agents/src/main/java/org/gpigf/presentation/db/AgentStore.java
@@ -50,7 +50,7 @@ public class AgentStore extends StaticMethodsProcessFactory<AgentStore> {
 
 		try {
 			Class.forName("org.postgresql.Driver"); 
-			String url = "jdbc:postgresql://localhost:5432/GPIGF"; 
+			String url = "jdbc:postgresql://localhost:5432/RoadsDB"; 
 			conn = DriverManager.getConnection(url, "postgres", "geoserver"); 
 			
 			//if(!(conn instanceof org.postgresql.jdbc4.Jdbc4Connection))
